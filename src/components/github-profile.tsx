fa62d79b67fa657266aafeec260cd4720e697064
Matheus <matheusim27@hotmail.com>
2025-07-30 10:11:04 -0300

    fix: dynamic git hub profile props

--- src/components/github-profile.tsx
+++ src/components/github-profile.tsx
@@ -1,12 +1,23 @@
-export function GithubProfile() {
+import type { github } from '@/lib/octokit'
+import { GetResponseTypeFromEndpointMethod } from '@octokit/types'
+
+type GithubUser = GetResponseTypeFromEndpointMethod<
+  typeof github.users.getByUsername
+>['data']
+
+interface GithubProfileProps {
+  user: GithubUser 
+}
+
+export function GithubProfile({ user }: GithubProfileProps) {
   return (
     <div className="bg-zinc-950 rounded-lg flex gap-3 p-6">
-      <img src="https://github.com/diego3g.png" alt="Diego Fernandes" className="size-10 rounded-full" />
+      <img src={user.avatar_url} alt={user.name ?? ''} className="size-10 rounded-full" />
 
       <div className="flex flex-col gap-1">
-        <span className="text-lg font-medium">Diego Fernandes</span>
+        <span className="text-lg font-medium">{user.name || user.login}</span>
         <p className="text-sm text-zinc-400 leading-relaxed">
-        CTO at @Rocketseat. Passionate about education and changing people's lives through programming.
+          {user.bio}
         </p>
       </div>
     </div>
